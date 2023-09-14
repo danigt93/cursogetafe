13d5db3b445b8e17b8d0f3b685b3d2034bcdb7ac
Tardes <Tardes@GET-03>
2023-09-14 17:54:13 +0200

    nuevo

---
+++ src/es/curso/java/ejercicio/CalculaEdad.java
@@ -0,0 +1,11 @@
+package es.curso.java.ejercicio;
+
+public class CalculaEdad{
+
+	public static void main (String[] args) {
+		
+		int calculaEdad(String anioNacimiento);
+		
+		void pintaTablaMultiplicar (int numero);
+	}
+}
