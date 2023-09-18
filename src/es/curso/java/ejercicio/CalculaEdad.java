9f05cbef8aafd7ca40ba2a913ef231142c06b99a
Tardes <Tardes@GET-03>
2023-09-18 16:16:31 +0200

    all new

--- src/es/curso/java/ejercicio/CalculaEdad.java
+++ src/es/curso/java/ejercicio/CalculaEdad.java
@@ -1,3 +1,5 @@
+
+
 package es.curso.java.ejercicio;
 
 import java.util.Scanner;
@@ -5,28 +7,45 @@ import java.util.Scanner;
 public class CalculaEdad{
 
 	public static void main (String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Dime tu año de nacimiento:");
-		int anioNacim = scan.nextInt();
-		int [] años = new int [anioNacim];
 		
+		//Para calcular la edad
+		String edad = "1993";
+		int numEdad = calculaEdad(edad);
+		System.out.println("El usuario tiene " + numEdad + " años");
 		
-		int anioAct = 2023;
+		//Tabla de multiplicar
+		pintaTablaMultiplicar();
 		
-		int calculaEdad = anioAct - anioNacim;
-		
-		System.out.println("El usuario tiene: " + calculaEdad);
-	
+		calculaVelocidad(1050,120);
 	}
+	
+
+	
+	
 	
 	static int calculaEdad(String anioNacimiento) {
-		 int anios = anioNacimiento.length();
-		 System.out.println(anios);
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Introduce tu año de nacimiento: ");
+		
+		int edadUsuario = scan.nextInt();
+		int anio = Integer.parseInt(anioNacimiento);
+		int anioReal = 2023 - edadUsuario;
+		return anioReal;
+		}
 	
+
+	
+	public static void pintaTablaMultiplicar() {
+		for(int i = 1; i<=10 ; i++ ) {
+			for(int j= 1; j<=10 ; j++) {
+				System.out.println(i + " X "+ j +" = " + (i*j));
+			}
+			System.out.println("=======TABLA DEL " + i +"=======");
+		}
 	}
 	
-	void pintaTablaMultiplicar (int numero) {
-		
+	public static void calculaVelocidad(double distancia, int tiempo) {
+		System.out.println("velocidad " + distancia / tiempo);
 	}
 }
--- src/es/curso/java/utilidades/Utilidades.java
+++ src/es/curso/java/utilidades/Utilidades.java
@@ -1,5 +1,35 @@
 package es.curso.java.utilidades;
 
+import java.util.Scanner;
+
 public class Utilidades {
 
+	public static void main(String[] args) {
+		String menu = "MENU";
+		String newMenu = menu(menu);
+		System.out.println(newMenu);
+	}
+	
+public static String menu (String texto) {
+	String opcion;
+	do {
+		System.out.println("1. Opcion 1\n" + "2. Opcion 2\n" + 
+				"3. Opcion 3\n" + "4. Opcion 4\n" +
+				"5. Opcion 5\n" + "6. Salir\n" );
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingresa la opcion que desees que aparezca en pantalla: ");
+		opcion = scan.nextLine();
+		
+		switch(opcion) {
+		case "1" : System.out.println("Opcion 1");break;
+		case "2" : System.out.println("Opcion 2");break;
+		case "3" : System.out.println("Opcion 3");break;
+		case "4" : System.out.println("Opcion 4");break;
+		case "5" : System.out.println("Opcion 5");break;
+		case "6" : System.out.println("Salir");break;
+		}
+	}while(Integer.parseInt(opcion)!=6);
+	return opcion;
+	
+ }
 }
