da73a4cf07e700706d6862c7bb31a6af1b9f4525
Tardes <Tardes@GET-03>
2023-10-04 19:24:42 +0200

    Se ha hacho un constructor con el SUPERCLASS desde "Source"

--- src/es/curso/java/poo/herencia/ClaseHija.java
+++ src/es/curso/java/poo/herencia/ClaseHija.java
@@ -4,6 +4,11 @@ public class ClaseHija extends ClasePadre {
 	
 	
 	
+	public ClaseHija(String dato1, int dato2) {
+		super(dato1, dato2);
+		
+	}
+
 	public void nombre () throws Exception{
 		
 	}
