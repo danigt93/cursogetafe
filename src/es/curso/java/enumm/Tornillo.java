6e2f5f4128ea18b4810c9c64565d56681961031e
Dani <correojl@gmail.com>
2024-01-24 19:18:13 +0100

    Actualización ejercicios y temario

--- src/es/curso/java/enumm/Tornillo.java
+++ src/es/curso/java/enumm/Tornillo.java
@@ -1,22 +1,13 @@
 package es.curso.java.enumm;
 
-public class TornilloEnum {
-
+public class Tornillo {
 	private long id;
 	private String color;
 	private double longitud;
 	private double diametro;
 	private double precio;
-
 	
-	
-	public TornilloEnum() {
-		super();
-		
-	}
-	
-	
-	public TornilloEnum(long id, String color, double longitud, double diametro, double precio) {
+	public Tornillo(long id, String color, double longitud, double diametro, double precio) {
 		super();
 		this.id = id;
 		this.color = color;
@@ -24,40 +15,79 @@ public class TornilloEnum {
 		this.diametro = diametro;
 		this.precio = precio;
 	}
-	
-	
-	
+
+	/**
+	 * @return the id
+	 */
 	public long getId() {
 		return id;
 	}
+
+	/**
+	 * @param id the id to set
+	 */
 	public void setId(long id) {
 		this.id = id;
 	}
+
+	/**
+	 * @return the color
+	 */
 	public String getColor() {
 		return color;
 	}
+
+	/**
+	 * @param color the color to set
+	 */
 	public void setColor(String color) {
 		this.color = color;
 	}
+
+	/**
+	 * @return the longitud
+	 */
 	public double getLongitud() {
 		return longitud;
 	}
+
+	/**
+	 * @param longitud the longitud to set
+	 */
 	public void setLongitud(double longitud) {
 		this.longitud = longitud;
 	}
+
+	/**
+	 * @return the diametro
+	 */
 	public double getDiametro() {
 		return diametro;
 	}
+
+	/**
+	 * @param diametro the diametro to set
+	 */
 	public void setDiametro(double diametro) {
 		this.diametro = diametro;
 	}
+
+	/**
+	 * @return the precio
+	 */
 	public double getPrecio() {
 		return precio;
 	}
+
+	/**
+	 * @param precio the precio to set
+	 */
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
 	
 	
 	
+	
 }
+
