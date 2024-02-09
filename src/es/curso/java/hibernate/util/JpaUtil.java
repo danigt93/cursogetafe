566e7d9b6d328be257420aa63c91555aca901d88
Dani <correojl@gmail.com>
2024-02-09 18:31:50 +0100

    act

--- src/es/curso/java/hibernate/util/JpaUtil.java
+++ src/es/curso/java/hibernate/util/JpaUtil.java
@@ -5,15 +5,15 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class JpaUtil { 
-//    private static final EntityManagerFactory entityManagerFactory = buildEntityManagerFactory();
-//
-//    private static EntityManagerFactory buildEntityManagerFactory(){
-//        return Persistence.createEntityManagerFactory("hibernateOracle");
-//    }
-//
-//    public static EntityManager getEntityManager() { 
-//        return entityManagerFactory.createEntityManager();
-//    }
+    private static final EntityManagerFactory entityManagerFactory = buildEntityManagerFactory();
+
+    private static EntityManagerFactory buildEntityManagerFactory(){
+        return Persistence.createEntityManagerFactory("hibernateOracle");
+    }
+
+    public static EntityManager getEntityManager() { 
+        return entityManagerFactory.createEntityManager();
+    }
     
     public static EntityManager getEM (String persistenceUnit) {
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnit);
